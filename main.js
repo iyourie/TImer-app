ea63d13c8c4eb1ccb49ae74be20c00608b52df19
iyourie <azhiedif@gmail.com>
2022-04-30 18:12:44 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let one = document.querySelectorAll('p')[0]
+let two = document.querySelectorAll('p')[1]
+let three = document.querySelectorAll('p')[2]
+let four = document.querySelectorAll('p')[3]
+let five = document.querySelectorAll('p')[4]
+
+
+let counter = setInterval(() => {
+
+    let goal = new Date('Apr 30, 2022 19:17:00').getTime()
+
+    let now = new Date().getTime()
+
+    let timer = goal - now
+
+    let months = Math.floor(timer / (1000 * 60 * 60 * 24 * 30))
+    let days = Math.floor(timer % (1000 * 60 * 60 * 24)/ 1000 / 60 / 60 / 24);
+    let hours = Math.floor(timer % (1000 * 60 * 60 * 24) / 1000 / 60 / 60);
+    let minutes = Math.floor(timer % (1000 * 60 * 60) / 1000 / 60);
+    let seconds = Math.floor(timer % (1000 * 60) / 1000);
+
+    one.textContent = seconds < 10 ? `0${seconds}` : seconds;
+    two.textContent = minutes < 10 ? `0${minutes}` : minutes;
+    three.textContent = hours < 10 ? `0${hours}` : hours;
+    four.textContent = days < 10 ? `0${days}` : days;
+    five.textContent = months < 10 ? `0${months}` : months;
+}, 1000);
+
+
